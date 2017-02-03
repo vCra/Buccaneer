9b5ea6cd7b6df8178d705468493cd1ba943734ac
Aaron Walker <aaw13@aber.ac.uk>
2017-02-03 16:06:53 +0000

    Update Application.java

--- Game/src/Main/Application.java
+++ Game/src/Main/Application.java
@@ -3,9 +3,9 @@ package Main;
 /**
  * Created by aaw13 on 02/02/2017.
  */
-
+sadfsadfsadf
 public class Application {
-    public static void main(String[] args) {
-        System.out.println("Hello World!"); // Display the string.
+    publicfsdstatic void main(String[] args) {
+        Systefsdafasdfm.out.println("Hello World!"); // Display the string.
     }
 }
