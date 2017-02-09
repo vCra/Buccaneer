52394f68c2a08d9e519c04777f2857138cfcad7f
vCra <aaw13@aber.ac.uk>
2017-02-09 12:08:00 +0000

    Update GameApp.java

--- Game/src/main/GameApp.java
+++ Game/src/main/GameApp.java
@@ -1,12 +1,11 @@
 package main;
 
-import islands.FlatIsland;
 
 /**
  * Created by aaw13 on 02/02/2017.
  */
 
-public class Application {
+public class GameApp {
     public static void main(String[] args) {
         Game game = new Game();
         game.begin();
