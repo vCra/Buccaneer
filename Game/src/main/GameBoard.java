66c0acbbfa10a7d0ce1eda56e7f4f2ba15b79306
Jakub Janas <jakub.janas08@gmail.com>
2017-02-14 15:49:09 +0000

    Added methods for GameApp, Game and GameSquare. Minor other changes

--- Game/src/main/GameBoard.java
+++ Game/src/main/GameBoard.java
@@ -3,6 +3,7 @@ package main;
 import islands.FlatIsland;
 import islands.PirateIsland;
 import islands.TreasureIsland;
+import ports.Port;
 import ports.HomePort;
 
 /**
@@ -12,22 +13,41 @@ import ports.HomePort;
 //TODO: Add ports to GameBoard
  class GameBoard {
     private GameSquare[][] gameSquares;
-    private HomePort[] ports;
+    private Port[] ports;
     private PirateIsland pirateIsland;
     private FlatIsland flatIsland;
     private TreasureIsland treasureIsland;
-    GameBoard() {
+    
+    /**
+     * Constructor. Creates an array of squares (20x20).
+     * Calls addSquares(), addPorts(), addIslands().
+     */
+    public GameBoard() {
         gameSquares = new GameSquare[20][20];
+        ports = new Port[6];
+        pirateIsland = new PirateIsland();
+        flatIsland = new FlatIsland();
+        treasureIsland = new TreasureIsland();
+        
         addSquares();
         addPorts();
         addIslands();
     }
+    
+    /**
+     * Adds Ports to the array.
+     * There are two generic ports only for trading
+     * and 4 HomePorts for players to store their treasure
+     * but also for everyone to trade.
+     */
     private void addPorts(){
         //TODO: Add ports to gameboard
     }
+    
     private void addSquares(){
         //TODO: Add squares to gameboard
     }
+    
     private void addIslands(){
         //TODO: Add Islands to gameboard
     };
--- Game/src/ports/Port.java
+++ Game/src/ports/Port.java
@@ -5,7 +5,7 @@ import main.GameObject;
  * Created by aaw13 on 02/02/2017.
  */
 //TODO: Add port methods and properties
-class Port implements GameObject {
+public class Port implements GameObject {
     //TODO: Store treasure and cards
     //TODO: Implement trading functionality
 }
