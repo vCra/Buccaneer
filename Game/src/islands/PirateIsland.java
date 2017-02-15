272a4b2193bc365bb12939bb24f42bff8aff78d3
Jakub Janas <jakub.janas08@gmail.com>
2017-02-15 14:53:36 +0000

    Added functionality to PirateIsland.

--- Game/src/islands/PirateIsland.java
+++ Game/src/islands/PirateIsland.java
@@ -1,7 +1,49 @@
 package islands;
 
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+import cards.CrewCard;
+import helpers.Position;
+
 /**
  * Created by awalker on 04/02/2017.
  */
-public class PirateIsland extends Island {
+public class PirateIsland extends Island
+{
+	private Queue<CrewCard> crewCardDeck;
+	
+	/**
+	 * Constructor.
+	 * Calls the super constructor with startPos and endPos.
+	 * Creates ArrayList for crewCardDeck.
+	 * @param startPos
+	 * @param endPos
+	 */
+	public PirateIsland (Position startPos, Position endPos)
+	{
+		super(startPos, endPos);
+		
+		crewCardDeck = new ArrayDeque<CrewCard>();
+	}
+
+	/**
+	 * Assigns a Queue of CrewCards to the crewCardDeck.
+	 * @param crewCardDeck
+	 */
+	public void setCrewCardDeck(Queue<CrewCard> crewCardDeck)
+	{
+		this.crewCardDeck = crewCardDeck;
+	}
+
+	/**
+	 * Returns a CrewCard from the front of the crewCardDeck.
+	 * @return crewCard
+	 */
+	public CrewCard getTopCard()
+	{
+		return crewCardDeck.poll();
+	}
+	
+	
 }
