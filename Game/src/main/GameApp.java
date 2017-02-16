c9f8c36679a2ca3dd86f131369403be266658497
vCra <aaw13@aber.ac.uk>
2017-02-16 11:27:33 +0000

    Resolved Conflicts

--- Game/src/main/GameApp.java
+++ Game/src/main/GameApp.java
@@ -28,55 +28,55 @@ public class GameApp extends Application {
         game.getPlayer(1).getPlayerShip().getLocation();
         window.setTitle("Group Project Demo");
 
-		Image water = new Image(getClass().getResource("/movingwater.gif").toURI().toString());
+        Image water = new Image(getClass().getResource("/movingwater.gif").toURI().toString());
 
-		ImageView imageview = new ImageView(water);
-		imageview.setFitWidth(800);
-		imageview.setFitHeight(800);
-		imageview.setPreserveRatio(true);
-		imageview.setSmooth(true);
-		imageview.setCache(true);
-		imageview.setMouseTransparent(true);
+        ImageView imageview = new ImageView(water);
+        imageview.setFitWidth(800);
+        imageview.setFitHeight(800);
+        imageview.setPreserveRatio(true);
+        imageview.setSmooth(true);
+        imageview.setCache(true);
+        imageview.setMouseTransparent(true);
 
-		GridPane gridpane = new GridPane();
-		for (int y = 0; y < 20; y++) {
-			for (int x = 0; x < 20; x++) {
-				ImageView tile = new ImageView();
-				tile.setFitWidth(40);
-				tile.setFitHeight(40);
-				tile.setPreserveRatio(true);
-				tile.setSmooth(true);
-				tile.setCache(true);
-				tile.setMouseTransparent(true);
-				GridPane.setRowIndex(tile, y);
-				GridPane.setColumnIndex(tile, x);
-				grid.add(tile);
-				gridpane.getChildren().add(tile);
-			}
-		}
-		
-		gridpane.setAlignment(Pos.CENTER);
+        GridPane gridpane = new GridPane();
+        for (int y = 0; y < 20; y++) {
+            for (int x = 0; x < 20; x++) {
+                ImageView tile = new ImageView();
+                tile.setFitWidth(40);
+                tile.setFitHeight(40);
+                tile.setPreserveRatio(true);
+                tile.setSmooth(true);
+                tile.setCache(true);
+                tile.setMouseTransparent(true);
+                GridPane.setRowIndex(tile, y);
+                GridPane.setColumnIndex(tile, x);
+                grid.add(tile);
+                gridpane.getChildren().add(tile);
+            }
+        }
 
-		StackPane stack = new StackPane();
-		stack.getChildren().addAll(imageview, gridpane);
-		
-		Scene scene = new Scene(stack, 1400, 800);
-		window.setScene(scene);
-		window.show();
-		
-		Image highlight = new Image(getClass().getResource("/highlight.jpg").toURI().toString());
-		
-		gridpane.addEventHandler(MouseEvent.MOUSE_PRESSED, new EventHandler<MouseEvent>() {
-			@Override
-			public void handle(MouseEvent e) {
-				for (Node node : gridpane.getChildren()) {
-					if (node.getBoundsInParent().contains(e.getSceneX(), e.getSceneY())) {
-						ImageView change = grid.get((GridPane.getRowIndex(node) * 20) + GridPane.getColumnIndex(node));
-						change.setImage(highlight);
-					}
-				}
-			}
-		});
-	}
+        gridpane.setAlignment(Pos.CENTER);
 
+        StackPane stack = new StackPane();
+        stack.getChildren().addAll(imageview, gridpane);
+
+        Scene scene = new Scene(stack, 1400, 800);
+        window.setScene(scene);
+        window.show();
+
+        Image highlight = new Image(getClass().getResource("/highlight.jpg").toURI().toString());
+
+        gridpane.addEventHandler(MouseEvent.MOUSE_PRESSED, new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent e) {
+                for (Node node : gridpane.getChildren()) {
+                    if (node.getBoundsInParent().contains(e.getSceneX(), e.getSceneY())) {
+                        ImageView change = grid.get((GridPane.getRowIndex(node) * 20) + GridPane.getColumnIndex(node));
+                        change.setImage(highlight);
+                    }
+                }
+            }
+        });
     }
+
+}
--- Game/src/main/Ship.java
+++ Game/src/main/Ship.java
@@ -2,7 +2,6 @@ package main;
 
 import enumData.Direction;
 import helpers.Position;
-import javafx.scene.image.Image;
 import treasure.Treasure;
 
 
@@ -14,21 +13,12 @@ public class Ship implements GameObject {
     private Player owner;
     private Position location;
     private Direction direction;
-    private Image image;
+
     public Ship(Player owner) {
         this.owner = owner;
         this.treasures = new Treasure[2];
-        this.setImage();
-
     }
 
-    private void setImage(){
-        try{
-            image = new Image(getClass().getResource("/pirateship.jpeg").toURI().toString());
-        } catch (java.net.URISyntaxException e){
-            System.err.println("Unable to find file");
-        }
-    }
     public Treasure[] getTreasures() {
         return treasures;
     }
