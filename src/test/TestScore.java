705143f442610e1e293aad4c16fb5f500dbdf9f0
vCra <aaw13@aber.ac.uk>
2017-03-02 14:09:41 +0000

    Add TestScore

---
+++ src/test/TestScore.java
@@ -0,0 +1,20 @@
+import buccaneer.helpers.Score;
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+
+/**
+ * Test the Score class
+ */
+public class TestScore {
+    @Test
+    public void testHasWon(){
+        Score score1 = new Score();
+        Score score2 = new Score();
+        Score score3 = new Score();
+        score1.setScore(20);
+        score2.setScore(21);
+        score3.setScore(19);
+        assertTrue(score1.hasWon() && score2.hasWon() && ! score3.hasWon());
+    }
+}
