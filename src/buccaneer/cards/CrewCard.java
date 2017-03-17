de86c8928fcf470f2dd44949df36234e48956a63
OutcastBelnor <jakub.janas08@gmail.com>
2017-03-17 16:15:09 +0000

    Add Image to CrewCards
    Added Image loading to CrewCards class and a Getter for Image

--- src/buccaneer/cards/CrewCard.java
+++ src/buccaneer/cards/CrewCard.java
@@ -1,6 +1,10 @@
 package buccaneer.cards;
 
 import buccaneer.enumData.CardColor;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 
 /**
  * Crew Card
@@ -12,11 +16,34 @@ public class CrewCard implements CardObject {
     private int id;
     private CardColor color;
     private int value;
+    private BufferedImage image;
 
     public CrewCard(int id, CardColor color, int value) {
         this.id = id;
         this.color = color;
         this.value = value;
+
+        loadImage();
+    }
+
+    private void loadImage()
+    {
+        int width = 186;
+        int height = 216;
+
+        image = null;
+        File f = null;
+
+        try
+        {
+            f = new File("res\\images\\crewcards\\CrewCard_" + color.toString() + value + ".png");
+            image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+            image = ImageIO.read(f);
+        }
+        catch (IOException ioe)
+        {
+            System.out.println("Error " + ioe);
+        }
     }
 
     public int getID() {
@@ -30,4 +57,6 @@ public class CrewCard implements CardObject {
     public int getValue() {
         return value;
     }
+
+    public BufferedImage getImage() { return image; }
 }
