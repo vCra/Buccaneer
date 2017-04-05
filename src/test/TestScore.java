6fdada912301b8704e6aec6271bfff4d6f292917
Adam-Lawson <adam.lawson96@hotmail.co.uk>
2017-04-05 18:52:17 +0000

    Added Score Junit Tests

--- src/test/TestScore.java
+++ src/test/TestScore.java
@@ -1,6 +1,7 @@
 import buccaneer.helpers.Score;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 
@@ -17,5 +18,28 @@ public class TestScore {
         score2.setScore(21);
         score3.setScore(19);
         assertTrue(score1.hasWon() && score2.hasWon() && !score3.hasWon());
+    }
+
+    @Test
+    public void testGetterandSetter() {
+        Score score = new Score();
+        score.setScore(10);
+        assertEquals(score.getScore(), 10);
+    }
+
+    @Test
+    public void testtoString() {
+        Score score = new Score();
+        score.setScore(10);
+        String string = score.toString();
+        assertEquals(string, "10");
+    }
+
+    @Test
+    public void testIncrementScore() {
+        Score score = new Score();
+        score.setScore(10);
+        score.addToScore(5);
+        assertEquals(score.getScore(), 15);
     }
 }
