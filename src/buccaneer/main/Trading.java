162d8aafd975bb9bb58dc04c3a3cbe77460c06e6
vCra <aaw13@aber.ac.uk>
2017-05-05 14:01:43 +0100

    Fix Giving a Crew card to a player actualy gives it to a port

--- src/buccaneer/main/Trading.java
+++ src/buccaneer/main/Trading.java
@@ -32,7 +32,11 @@ public class Trading {
                 port.getTreasures().add((Treasure) t);
                 player.getPlayerShip().removeTreasure((Treasure) t);
             } else if (t instanceof CrewCard) {
-                port.getCrewCards().add((CrewCard) t);
+                if (port.isOwned()) {
+                    port.getOwner().addCrewCard((CrewCard) t);
+                } else {
+                    port.getCrewCards().add((CrewCard) t);
+                }
                 player.getCrewCards().remove(t);
             }
         }
