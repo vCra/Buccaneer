2676db72526c34590889ed9d282d985bc4659f06
kermitsoncrackk <liamevans18@hotmail.co.uk>
2017-05-05 13:46:35 +0100

    Additional Test

--- src/test/PositionHelperTest.java
+++ src/test/PositionHelperTest.java
@@ -7,9 +7,9 @@ import buccaneer.main.Ship;
 import buccaneer.ports.Port;
 import org.junit.Test;
 
+import static buccaneer.enumData.CardColor.Black;
 import static buccaneer.enumData.CardColor.Red;
-import static buccaneer.enumData.Direction.E;
-import static buccaneer.enumData.Direction.N;
+import static buccaneer.enumData.Direction.*;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
@@ -104,25 +104,119 @@ public class PositionHelperTest {
 
 
     }
-
     @Test
-    public void checkporthighlight3()
+    public void shipstrengthinportTest()          //????  Change Port
     {
+
         int size1 = 0;
+        int size2 = 0;
+
         GameBoard gb = new GameBoard();
         CrewCard cd = new CrewCard(1, Red,3);
+        CrewCard cd1 = new CrewCard(2,Red,3);
+        CrewCard cd2 = new CrewCard(3,Red,3);
+        CrewCard cd3 = new CrewCard(4,Red,1);
         Player p1 = new Player(0,"1");
-        Ship s1= new Ship(p1);
-        ports.add(new Port("London", getSquareAt(1, 14)));
+        Player p2 = new Player(1,"2");
 
+        Ship s1= new Ship(p1);
+        Ship s2= new Ship(p2);
 
         s1.setLocation(new GameSquare(1,14,gb));
+        s2.setLocation(new GameSquare(1,7,gb));
+
+
         s1.setDirection(E);
+        s2.setDirection(E);
+
         p1.addCrewCard(cd);
+        p1.addCrewCard(cd1);
+        p1.addCrewCard(cd2);
+        p1.addCrewCard(cd3);
+
+        p2.addCrewCard(cd2);
 
         PositionHelper.getAvailableMoves(s1);
-        size1 = PositionHelper.getAvailableMoves(s1).size();
-        assertEquals(size1, 47);
+        PositionHelper.getAvailableMoves(s2);
+
+        size1 = PositionHelper.getAvailablePortMoves(s1).size();
+        size2 = PositionHelper.getAvailablePortMoves(s2).size();
+
+        assertEquals(size1, 37);
+        assertEquals(size2, 15);
+
+
+
+
+    }
+
+    @Test
+    public void checkeachplayersmoves()
+    {
+        int p1size = 0;
+        int p2size = 0;
+        int p3size = 0;
+        int p4size = 0;
+
+        GameBoard gb = new GameBoard();
+        CrewCard cd1 = new CrewCard(1, Red,3);
+        CrewCard cd2 = new CrewCard(2, Red,2);
+        CrewCard cd3 = new CrewCard(3, Red,1);
+        CrewCard cd4 = new CrewCard(4, Red,3);
+        CrewCard cd5 = new CrewCard(5, Red,2);
+
+        CrewCard cd6 = new CrewCard(5, Black,2);
+        CrewCard cd7 = new CrewCard(5, Black,3);
+
+        Player p1 = new Player(0,"1");
+        Player p2 = new Player(1,"2");
+        Player p3 = new Player(2,"3");
+        Player p4 = new Player(3,"4");
+        Ship s1= new Ship(p1);
+        Ship s2= new Ship(p2);
+        Ship s3= new Ship(p3);
+        Ship s4= new Ship(p4);
+        //ports.add(new Port("London", getSquareAt(1, 14)));
+
+
+        s1.setLocation(new GameSquare(2,14,gb));
+        s2.setLocation(new GameSquare(20,8,gb));
+        s3.setLocation(new GameSquare(6,2,gb));
+        s4.setLocation(new GameSquare(14,1,gb));
+
+        s1.setDirection(E);
+        s2.setDirection(N);
+        s3.setDirection(NE);
+        s4.setDirection(S);
+
+        p1.addCrewCard(cd1);
+        p2.addCrewCard(cd2);
+        p3.addCrewCard(cd1);
+        p4.addCrewCard(cd3);
+        p1.addCrewCard(cd1);
+        p2.addCrewCard(cd5);
+        p3.addCrewCard(cd1);
+        p4.addCrewCard(cd4);
+        p1.addCrewCard(cd7);
+        p2.addCrewCard(cd3);
+        p3.addCrewCard(cd7);
+        p4.addCrewCard(cd2);
+        p1.addCrewCard(cd3);
+        p2.addCrewCard(cd6);
+        p3.addCrewCard(cd1);
+        p4.addCrewCard(cd2);
+
+        //PositionHelper.getAvailableMoves(s1);
+
+        p1size = PositionHelper.getAvailableMoves(s1).size();
+        p2size = PositionHelper.getAvailableMoves(s2).size();
+        p3size = PositionHelper.getAvailableMoves(s3).size();
+        p4size = PositionHelper.getAvailableMoves(s4).size();
+
+        assertEquals(p1size, 10);
+        assertEquals(p2size, 7);
+        assertEquals(p3size, 12);
+        assertEquals(p4size, 0);
 
 
     }
