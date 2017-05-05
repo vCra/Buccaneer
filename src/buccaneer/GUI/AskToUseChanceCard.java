f03079229495bd73dd2cf5363996789a287055c7
Adam-Lawson <adam.lawson96@hotmail.co.uk>
2017-05-05 11:47:11 +0000

    Changed would you like to use chance card from text to new images

--- src/buccaneer/GUI/AskToUseChanceCard.java
+++ src/buccaneer/GUI/AskToUseChanceCard.java
@@ -6,6 +6,7 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Modality;
@@ -40,9 +41,9 @@ public class AskToUseChanceCard {
         window.setTitle("Use The Chance Card?");
 
         Label title = new Label("Would you like to use Chance Card " + Integer.toString(chanceCard.getID()));
-        Label text = new Label(chanceCard.getText());
-        text.setMaxWidth(300);
-        text.setWrapText(true);
+        ImageView chanceCardText = new ImageView(chanceCard.getTextImage());
+        chanceCardText.setFitHeight(400);
+        chanceCardText.setFitWidth(300);
         Button yes = new Button("Yes");
         Button no = new Button("No");
 
@@ -62,7 +63,7 @@ public class AskToUseChanceCard {
         buttons.getChildren().addAll(yes, no);
         buttons.setAlignment(Pos.CENTER);
         VBox layout = new VBox(20);
-        layout.getChildren().addAll(title, text, buttons);
+        layout.getChildren().addAll(title, chanceCardText, buttons);
         layout.setAlignment(Pos.CENTER);
 
         Scene scene = new Scene(layout, 600, 600);
